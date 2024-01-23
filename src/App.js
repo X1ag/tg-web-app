5402462ccd555cd50d09391571bd279c849d0734
X1ag <super.don17080@gmail.com>
2024-01-23 23:14:58 +0300

    Debags

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { useEffect } from 'react'
 import './App.css'
-import { useTelegram } from './components/hooks/useTelegram'
+import { useTelegram } from './components/hooks/useTelegram.js'
 
 function App() {
-  const {onToggleButton,tg} = useTelegram();
-  useEffect(()=>{
-    tg.ready();
-  }, [])
-  
+	const { onToggleButton, tg } = useTelegram()
+	useEffect(() => {
+		tg.ready();
+	}, [])
 
 	return (
-  <div className='App'>
-    <button onClick={onToggleButton}>toggle</button>
-  </div>
-  );
+		<div className='App'>
+			<button onClick={onToggleButton}>toggle</button>
+		</div>
+	)
 }
 
 export default App
