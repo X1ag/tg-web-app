d960dd137ebadfc19784e878b5f3eccde72b3993
X1ag <super.don17080@gmail.com>
2024-01-24 12:26:43 +0300

    Version Control is so laggy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
 import { useEffect } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
 import { useTelegram } from './components/hooks/useTelegram'
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
 
 	return (
 		<div className='App'>
+			<Header />
 			<button onClick={onToggleButton}>toggle</button>
 		</div>
 	)
